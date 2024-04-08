c4f7452cc51c591ee6a93b580ba206fd96b2fbfc
niksharma0910 <sharma.nikhil334@gmail.com>
2024-04-08 12:01:15 +0530

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,10 @@
+public class multiply{
+	public static void main(String args[]){
+		int a = 6;
+		int b = 4;
+
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+		System.out.println(a*b);
+	}
+}
